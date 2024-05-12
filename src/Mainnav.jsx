1278ab3bb13923a37f41a9012ccd6acb3c888f6a
geonsoon <b_b28@naver.com>
2024-05-12 22:11:00 +0900

    컴포넌트

---
+++ src/Mainnav.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Mainnav extends Component {
+    render() {
+        return (
+            <div>
+                123456
+            </div>
+        );
+    }
+}
+
+export default Mainnav;
