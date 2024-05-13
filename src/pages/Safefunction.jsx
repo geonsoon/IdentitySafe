5e6a2f60e751b3c920fda9a5526ba36cf262099a
geonsoon <b_b28@naver.com>
2024-05-13 14:26:11 +0900

    footer

--- src/pages/Safefunction.jsx
+++ src/pages/Safefunction.jsx
@@ -8,11 +8,11 @@ import './Safefunction.css';
 class Safefunction extends Component {
     render() {
         return (
-            <div className='center'>
+            <div className='footer'>
                 <span>개인정보비식별화 대상을 선택하세요</span>
                 <div className='icon'>
-                <FontAwesomeIcon icon={faCameraRetro} size="lg" />
-                <FontAwesomeIcon icon={faImage} size="lg" />
+                <span className='margin'><FontAwesomeIcon icon={faCameraRetro} size="2xl" /></span>
+                <span><FontAwesomeIcon icon={faImage} size="2xl" /></span>
                 </div>
             </div>
         );
