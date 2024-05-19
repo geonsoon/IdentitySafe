4f881cadccbdb7bda1d56722d05a9afd352486e2
geonsoon <b_b28@naver.com>
2024-05-19 16:44:59 +0900

    카메라 제거

--- camera/README.md
+++
@@ -1,2 +0,0 @@
-# react-video-recorder
-Created with CodeSandbox
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { StrictMode } from "react";
 import './index.css';
 import App from './App';
 
